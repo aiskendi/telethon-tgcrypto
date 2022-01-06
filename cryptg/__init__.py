ebc973c2aa1da73b1bac983d2569153b9990184b
aiskendi <96066438+aiskendi@users.noreply.github.com>
2022-01-07 02:54:09 +0800

    Update __init__.py

--- cryptg/__init__.py
+++ cryptg/__init__.py
@@ -1,3 +1,10 @@
 from tgcrypto import ige256_decrypt as decrypt_ige
 from tgcrypto import ige256_encrypt as encrypt_ige
-__all__ = ["decrypt_ige", "encrypt_ige"]
+from tgcrypto import ctr256_encrypt as encrypt_ctr
+from tgcrypto import ctr256_decrypt as decrypt_ctr
+__all__ = [
+    "decrypt_ige",
+    "encrypt_ige",
+    "encrypt_ctr",
+    "decrypt_ctr"
+    ]
